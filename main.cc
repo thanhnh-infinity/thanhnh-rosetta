1a1653004aaef65a42dbe169e263f29b799c4518
thanhnh-infinity <thanhnh2911@gmail.com>
2017-05-09 01:54:59 -0600

    Update

--- main.cc
+++ main.cc
@@ -102,6 +102,36 @@ std::string getPrecisionValue(double number,int pre){
 }
 
 
+template<class ForwardIt>
+ForwardIt unique(ForwardIt first, ForwardIt last)
+{
+    if (first == last)
+        return last;
+
+    ForwardIt result = first;
+    while (++first != last) {
+        if (!(*result == *first) && ++result != first) {
+            *result = std::move(*first);
+        }
+    }
+    return ++result;
+}
+
+template<class ForwardIt, class BinaryPredicate>
+ForwardIt unique(ForwardIt first, ForwardIt last, BinaryPredicate p)
+{
+    if (first == last)
+        return last;
+
+    ForwardIt result = first;
+    while (++first != last) {
+        if (!p(*result, *first) && ++result != first) {
+            *result = std::move(*first);
+        }
+    }
+    return ++result;
+}
+
 int main(int argc, char* argv[])
 {
   RosettaInput rosinput;
